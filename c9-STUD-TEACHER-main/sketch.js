a6cb800b5838aeb18753c18e9410fe13e2ddeab6
Mjalloh23 <89691774+Mjalloh23@users.noreply.github.com>
2021-08-28 13:23:30 +0100

    Add files via upload

---
+++ c9-STUD-TEACHER-main/sketch.js
@@ -0,0 +1,15 @@
+
+
+var a = prompt('Enter the first variable: ');
+
+var b = prompt('Enter the second variable: ');
+
+function setup(){
+  var b2 = createButton("click here to Swap");
+
+  }
+
+
+function draw()
+{
+}
